eb6c8190ad3b43becf9a8b00f85b113e69c7b000
mayur_007 <mayurmyana111@gmail.com>
2026-01-07 10:59:19 +0530

    feat: Implement forgotten password functionality with email-based token reset and security configuration.

--- src/main/java/com/api/ChalooApplication.java
+++ src/main/java/com/api/ChalooApplication.java
@@ -1,5 +1,6 @@
 package com.api;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,6 +13,16 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class ChalooApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.directory("./")
+				.ignoreIfMalformed()
+				.ignoreIfMissing()
+				.load();
+
+		dotenv.entries().forEach(entry -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
+
 		SpringApplication.run(ChalooApplication.class, args);
 	}
 
